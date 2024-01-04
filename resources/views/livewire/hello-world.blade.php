61a6127f803c9c1ae2a75505e21ca4a5b1ceebe4
Marvin Galdamez <marvin.galdamez@movistar.com.sv>
2024-01-03 19:05:15 -0600

    initial commit

---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,7 @@
+<div>
+    The current time is: {{time()}}
+
+    <button wire:click="$refresh">Refresh</button>
+
+
+</div>
---
+++ resources/views/livewire/todos.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <form wire:submit="add">
+        <input type="text" wire:model="todo">
+        <!-- podemos usar wire:model.live para actualizar en vivo el backend -->
+
+
+
+        <button type="Submit">Add</button>
+    </form>
+    <ul>
+        @foreach ($todos as  $todo)
+            <li>{{$todo}}</li>
+        @endforeach
+    </ul>
+</div>
